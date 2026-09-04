6c37f0aaa11f7496d54ee5cadc0ec17a6db6e5cd
sahnk3511-max <sahnk3511@gmail.com>
2026-09-05 01:19:48 +0300

    Change status to Streaming - SAM

--- bot.py
+++ bot.py
@@ -16,8 +16,8 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user} متصل الآن! ✅')
-    # تعيين حالة البوت
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="SAM"))
+    # تعيين حالة البوت - Streaming
+    await bot.change_presence(activity=discord.Streaming(name="SAM", url="https://www.twitch.tv/"))
 
 # أمر hello
 @bot.command()
